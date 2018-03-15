4e685185acece915c0a51209b2302d5b6c7bfcb8
Pranay Ankit <pranayankit414@gmail.com>
2018-03-15 10:13:34 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports = (robot) => {
+  // Your code here
+  robot.log('Yay, the app was loaded!')
+  robot.on('issues.opened', async context => {
+    const params = context.issue({body: 'Thanks for reporting! We\'ll look into this'});
+
+    //Post comment
+    return context.github.issues.createComment(params);
+  });
+
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+}
