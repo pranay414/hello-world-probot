9a6936359ee8e36969b6064cc2d9173ad65792b7
Pranay Ankit <pranayankit414@gmail.com>
2018-03-16 09:46:17 +0530

    Added: create help wanted label when an issue is opened

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ module.exports = (robot) => {
   robot.log('Yay, the app was loaded!')
   robot.on('issues.opened', async context => {
     const params = context.issue({body: 'Thanks for reporting! We\'ll look into this'});
-
     //Post comment
     return context.github.issues.createComment(params);
+  });
+  robot.on('issues.opened', async context => {
+    const params = context.issue({name: 'help wanted', color: '3BB273'});
+    //Create label
+    return context.github.issues.createLabel(params);
   });
 
   // For more information on building apps:
