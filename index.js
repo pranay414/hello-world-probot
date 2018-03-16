156347414e864904347758f5e2c4e29d3636c2d9
Pranay Ankit <pranayankit414@gmail.com>
2018-03-16 09:54:57 +0530

    Modified: index.js to attach labels only

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 module.exports = (robot) => {
   // Your code here
   robot.log('Yay, the app was loaded!')
-  robot.on('issues.opened', async context => {
+/*   robot.on('issues.opened', async context => {
     const params = context.issue({body: 'Thanks for reporting! We\'ll look into this'});
     //Post comment
     return context.github.issues.createComment(params);
-  });
+  }); */
   robot.on('issues.opened', async context => {
     const params = context.issue({name: 'help wanted', color: '3BB273'});
     //Create label
