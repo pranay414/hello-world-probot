0aa3c07a818d1c382ba2821a134a7648814d529d
Pranay Ankit <pranayankit414@gmail.com>
2018-03-21 20:26:48 +0530

    testing auto assing issues

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ module.exports = (robot) => {
   //Listen for commands like /assign
   commands(robot,'assign',(context,command) => {
     //return context.github.issues.addLabels(context.issue({labels: ['assigned']}))
-    console.log(`payload: ${context.payload}`)
+    robot.log(`payload: ${context.payload}`)
     return context.github.issues.addAssigneesToIssue(context.issue({assignees: [`pranay414`]}))
   })
   // For more information on building apps:
